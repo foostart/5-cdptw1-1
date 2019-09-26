b987bf1ecb88b757312ed1279e8f06362376e29f
dchinh99 <gameharb04@gmail.com>
2019-09-26 14:21:56 +0700

    5 module-update
    up

--- 1975/1975-content.php
+++ 1975/1975-content.php
@@ -6,7 +6,6 @@ preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-
 <div class="type-2014">  
     <footer id="footer">
         <div class="footer-container">
